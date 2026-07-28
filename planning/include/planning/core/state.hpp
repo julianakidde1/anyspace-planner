9c75fa5694ebb09ce9fa11fa59a12cecfa0bd3b1
Juliana Kidde <julianamwesigwa@gmail.com>
2026-07-28 10:10:34 -0500

    Add State struct for unicycle agents

--- planning/include/planning/core/state.hpp
+++ planning/include/planning/core/state.hpp
@@ -1,1 +1,10 @@
-// state struct
+namespace planning {
+    struct State {
+        double x = 0.0;
+        double y = 0.0;
+        double theta = 0.0; //orientation
+        double v = 0.0;  //forward velocity
+        double omega = 0.0; //angular velocity
+    };
+
+}
