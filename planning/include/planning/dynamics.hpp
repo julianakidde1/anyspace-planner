31228ba377ab77591e209f1d14bd955ac1844c30
Juliana Kidde <julianamwesigwa@gmail.com>
2026-09-08 16:39:11 -0500

    Add Controller{} to Dynamics

--- planning/include/planning/dynamics.hpp
+++ planning/include/planning/dynamics.hpp
@@ -1,1 +1,11 @@
-//dynamics
+#include  <cmath>
+#include "state.hpp"
+
+namespace planning {
+
+    struct Controller {
+        double v = 0.0; //forward speed (m/s)
+        double omega = 0.0; // yaw rate (rad/s)
+    };
+
+} //namespace planning
